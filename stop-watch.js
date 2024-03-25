ab567cfa48ecce0d2320a398b8b3b7225f2086f4
dev-world <hassamleague08@gmail.com>
2024-03-26 01:54:27 +0400

    some better code

--- stop-watch.js
+++ stop-watch.js
@@ -3,53 +3,69 @@ let startbtn = document.getElementById('startbtn')
 let stopbtn = document.getElementById('stopbtn')
 let resetbtn = document.getElementById('resetbtn')
 
-let msec = 0;
-let sec  = 0;
-let minute = 0;
+let msec = '0'
+let sec  = '0'
+let minute = '0'
 
-let timerId = null;
+startbtn.addEventListener('click',()=>{
 
-startbtn.addEventListener('click',function(){
-      
-    if(timerId !== null){
-        clearInterval(timerId);
-  }
-  timerId = setInterval(starttimer, 10)
 
-    });
 
-    stopbtn.addEventListener('click',function(){
-      
-            clearInterval(timerId);
+ clear1 = setInterval(()=>{
+
+    if(msec<59){
+
+    msec++
+
+}
+
+
+
+else{
+    msec=0
+    sec++
+}
+
+if(sec==60){
+
+    sec=0
+    minute++
     
-        });
-
-        resetbtn.addEventListener('click',function(){
-      
-            clearInterval(timerId);
-            timerdisplay.innerHTML = `00 : 00 : 00`;
-            msec = sec = minute = 0; 
-        });
-
-
-function starttimer(){
-    msec ++;
-    if(msec == 100){
-       msec = 0;
-
-       sec++;
-    if(sec == 60){
-       sec = 0;
-
-       minute ++;
-         
     }
 
-    }
- 
-    let msecString = msec < 10 ? `0${msec}` : msec;
-    let secString = sec < 10 ? `0${sec}` : sec;
-    let minuteString = minute < 10 ? `0${minute}` : minute;
+let minuteString = minute < 10 ? `0${minute}` : minute;
+let secString = sec < 10 ? `0${sec}` : sec;
+let msecString = msec < 10 ? `0${msec}` : msec;
 
-    timerdisplay.innerHTML = `${minuteString} : ${secString} : ${msecString}`
-}   
+
+timerdisplay.innerHTML=`${minuteString}:${secString}:${msecString}`
+},10)
+
+
+
+})
+
+
+
+ stopbtn.addEventListener('click',()=>{
+
+clearInterval(clear1)
+
+ })
+
+
+ resetbtn.addEventListener('click',()=>{
+
+clearInterval(clear1)
+    minute=0
+   sec=0
+
+   timerdisplay.innerHTML='00:00:00'
+
+    
+     })
+
+
+
+
+//
